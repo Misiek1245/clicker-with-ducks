0f3bddf6721d14311281c72f0d4b341fb46f69ff
Misiek1245 <embappe7@gmail.com>
2025-12-30 21:55:30 +0100

    reapering smthing small and finishing all of the ducks

--- scripts.js
+++ scripts.js
@@ -7,65 +7,59 @@ const shopDuck = document.getElementById("shop_duck");
 
 const shopMap = {
     "img/duck1.png":{
-        preview :"img/.duck2.png",
+        preview :"img/duck2s.png",
         next: "img/duck2.png",
         price : 10,
         mult : 1
     } ,
     "img/duck2.png":{ 
-        preview: "img/.duck3.png",
+        preview: "img/duck3s.png",
         next: "img/duck3.png",
         price : 100
         ,mult : 2
     },
     "img/duck3.png":{ 
-        preview: "img/.duck4.png",
+        preview: "img/duck4s.png",
         next: "img/duck4.png",
         price : 500
         ,mult : 5
     },
     "img/duck4.png":{ 
-        preview: "img/.duck5.png",
+        preview: "img/duck5s.png",
         next: "img/duck5.png",
         price : 1000
         ,mult : 10
     },
     "img/duck5.png":{ 
-        preview: "img/.duck6.png",
+        preview: "img/duck6s.png",
         next: "img/duck6.png",
         price : 2500
         ,mult : 15
     },
     "img/duck6.png":{ 
-        preview: "img/.duck7.png",
+        preview: "img/duck7s.png",
         next: "img/duck7.png",
         price : 5000
         ,mult : 30
     },
     "img/duck7.png":{ 
-        preview: "img/.duck8.png",
+        preview: "img/duck8s.png",
         next: "img/duck8.png",
         price : 7500
         ,mult : 50
     },
     "img/duck8.png":{ 
-        preview: "img/.duck9.png",
+        preview: "img/duck9s.png",
         next: "img/duck9.png",
         price : 10000
         ,mult : 100
     },
     "img/duck9.png":{ 
-        preview: "img/.duck10.png",
+        preview: "img/duck10s.png",
         next: "img/duck10.png",
         price : 50000
         ,mult : 150
-    },"img/duck310.png":{ 
-        preview: "img/the_end.png",
-        next: "img/duck4.png",
-        price : 100000
-        ,mult : 300
-    }
-    
+    }   
 
 };
 
