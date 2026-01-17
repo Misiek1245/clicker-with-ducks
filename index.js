6a22c63a247f56255ba6056dc8c75d1fa660f41d
Miskek <embappe7@gmail.com>
2026-01-17 10:07:18 +0100

    small change

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const {app, BrowserWindow } = require('electron');
+const path = require("path");
 
 function create_window(){
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 1000,
         frame: false,
+        icon:path.join(__dirname, "img", "logo.ico"),
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
@@ -15,5 +17,5 @@ function create_window(){
 }
 app.whenReady().then(create_window)
 app.on('window-all-closed',() => {
-    if (process.platform !== 'darwin')app.quit
+    if (process.platform !== 'darwin')app.quit()
 })
